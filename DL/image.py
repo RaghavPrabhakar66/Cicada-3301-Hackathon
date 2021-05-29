f0e20eae278609e563a38a3d641ac89d6af824f9
Raghav Prabhakar <52318784+RaghavPrabhakar66@users.noreply.github.com>
2021-05-29 17:45:32 +0530

    Update image.py

--- DL/image.py
+++ DL/image.py
@@ -1,3 +1,6 @@
+from PIL import Image
+from tqdm import tqdm
+import os
 import torch
 import torch.nn as nn
 import torchvision.models as models
@@ -5,27 +8,18 @@ import torchvision.transforms as transforms
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+from torch.autograd import Variable
+from torch.nn import Module
 
 class Model(Module):
-    """
-    CNN encoder using pretrained models
-    """
     def __init__(self, backbone):
         super(Model, self).__init__()
         self.base_model = models.inception_v3(pretrained=True)
-        print(self.base_model)
         self.base_model.fc = nn.Identity()
 
-    # Foward pass
-    def forward(self, input):
-        x = self.base_model(input)
+    def forward(self, input): return self.base_model(input)
 
-        return x
-from PIL import Image
-from tqdm import tqdm
-import os
-
-class Img2Vec:
+class ImgFilter:
     def __init__(self, size, device='auto'):
         '''
         Arguments:
@@ -47,6 +41,9 @@ class Img2Vec:
 
         self.model = None
             
+    def process_image(self, img):
+        return Variable(self.normalize(self.to_tensor(self.resize(img))).unsqueeze(0))
+    
     def get_image(self, path):
         img = Image.open(path)
         img_processed = Variable(self.normalize(self.to_tensor(self.resize(img))).unsqueeze(0))
@@ -63,26 +60,39 @@ class Img2Vec:
 
     def get_vectors(self, img_processed):
         return self.model(img_processed)
-    
-    def run(self, path1, path2):
-        img1 = self.get_image(path1)
-        img2 = self.get_image(path2)
 
+    def generate_np(self, label, files):
         self.model = self.get_model('vgg16')
         self.model.eval()
-        
-        final = []
-        for i in tqdm(range(5)):
-            vec1 = self.get_vectors(img1).squeeze(0)
-            vec2 = self.get_vectors(img2).squeeze(0)
-            print(vec1.shape)
-            cos = self.cosine_similarity(vec1, vec2)
-            final.append(cos.detach().numpy())
-        print('\n',sum(final)/len(final))
-  
-  if __name__ == '__main__':
-    img2vec = Img2Vec(224, 'auto')
-    path1 = 'Path of Image 1'
-    path2 = 'Path of Image 2'
 
-    img2vec.run(path1, path2)
+        final = []
+        for i in range(3):
+            path = '/content/seg_train/seg_train/' + label + '/' + files[i]
+            print(path)
+            img = self.get_image(path)
+            vec = self.get_vectors(img).squeeze(0)
+            final.append(vec.detach().numpy())
+        mean = np.mean(final, axis=0)
+        np.save(f'{label}', mean)
+    
+    def run(self, embed_file_path, img):
+        self.model = self.get_model('vgg16')
+        self.model.eval()
+        stored_embed = None
+
+        img = self.process_image(img)
+
+        if os.path.isfile(embed_file_path):
+            stored_embed = np.load(embed_file_path)
+            vec = self.get_vectors(img).squeeze(0)
+            cos = self.cosine_similarity(torch.from_numpy(stored_embed), vec)
+            mean = np.mean([stored_embed, vec.detach().numpy()], axis=0)
+            return (True, mean) if cos >= 0.50 else (False, mean)
+        else:
+            print ("File not exist")
+            exit()
+
+if __name__ == '__main__':
+    img_filter = ImgFilter(224, 'auto')
+    img = Image.open('/content/seg_train/seg_train/forest/18363.jpg')
+    res, mean = img_filter.run('/content/forest.npy', img)
